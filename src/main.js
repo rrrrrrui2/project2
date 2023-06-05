542a89e93dc4f09a9d3e37fee21154a6f4e197ca
lusirui <1670679130@qq.com>
2023-06-05 21:46:00 +0800

    manager

---
+++ src/main.js
@@ -0,0 +1,46 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Vuetify from 'vuetify'
+import config from './config'
+import MyComponent from './components/MyComponent'
+import './http';
+import 'vuetify/dist/vuetify.min.css'
+import qs from 'qs'
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/material.css'
+import Vuex from 'vuex'
+
+Vue.use(Vuetify, { theme: config.theme})
+Vue.use(MyComponent)
+Vue.prototype.$qs = qs;
+
+Vue.config.productionTip = false
+
+Vue.use(Vuex)
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  },
+  actions: {
+    increment(context) {
+      context.commit('increment')
+    }
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>',
+  store,
+})
---
+++ src/pages/item/Category.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-card>
+      <v-flex xs12 sm10>
+        <v-tree url="/item-services/category/list"
+                :isEdit="isEdit"
+                @handleAdd="handleAdd"
+                @handleEdit="handleEdit"
+                @handleDelete="handleDelete"
+                @handleClick="handleClick"
+        />
+      </v-flex>
+  </v-card>
+</template>
+
+<script>
+  import {treeData} from "../../mockDB";
+
+  export default {
+    name: "category",
+    data() {
+      return {
+        isEdit:true,
+       /* treeData*/
+      }
+    },
+    methods: {
+      handleAdd(node) {
+        console.log("add .... ");
+        console.log(node);
+        this.$http.get("/item-services/category/add",{
+          params:{
+            node
+          }
+        }).then(() => {
+          this.$message.success("添加成功！");
+        })
+          .catch(() => {
+            this.$message.error("添加失败！");
+          });
+      },
+      handleEdit(id, name) {
+        console.log("edit... id: " + id + ", name: " + name)
+        this.$http.get("/item-services/category/edit",{
+          params:{
+            id,
+            name
+          }
+        }).then(() => {
+          this.$message.success("修改成功！");
+        })
+          .catch(() => {
+            this.$message.error("修改失败！");
+          });
+      },
+      handleDelete(id) {
+        console.log("delete ... " + id)
+        this.$http.get("/item-services/category/delete",{
+          params:{
+            id,
+          }
+        }).then(() => {
+          this.$message.success("删除成功！");
+        })
+          .catch(() => {
+            this.$message.error("删除失败！");
+          });
+      },
+      handleClick(node) {
+        console.log(node)
+      }
+    }
+  };
+</script>
+
+<style scoped>
+
+</style>
